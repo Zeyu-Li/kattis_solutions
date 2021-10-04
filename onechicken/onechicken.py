413c906cc37be9770b092cfccf77dd355b790811
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-04 10:34:55 -0600

    chicken 😋

---
+++ onechicken/onechicken.py
@@ -0,0 +1,11 @@
+def main():
+    n, m = map(int, input().split())
+
+    if n < m:
+        print(f"Dr. Chaz will have {m-n} piece{'' if m-n == 1 else 's'} of chicken left over!")
+    else:
+        print(f"Dr. Chaz needs {n-m} more piece{'' if n-m == 1 else 's'} of chicken!")
+    return
+
+if __name__ == "__main__":
+    main()
